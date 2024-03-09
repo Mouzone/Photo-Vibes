eb2adbc0f523d8a90129a5d845c77c2c7ed05ce7
Mouzone <sunnyliu010@gmail.com>
2024-03-08 20:00:19 -0500

    accessing image's metadata: mode, size, filetype

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ with Image.open(filename) as img:
 print(f"Type: {type(img)}")
 
 print(f"is img an instance of Image.Image? {isinstance(img, Image.Image)}")
+
+img.show()
+
+print(f"format: {img.format}")
+print(f"size: {img.size}")
+print(f"mode: {img.mode}")
