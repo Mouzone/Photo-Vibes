8d63532b438b10656db9010645fb36bd41818ab3
Mouzone <sunnyliu010@gmail.com>
2024-03-13 20:30:36 -0400

    assets, and base guidelines for album_art project

---
+++ album_art/main.py
@@ -0,0 +1,3 @@
+# make it able to take a file name input or some kind of drag and drop website to proccess
+
+# output is the edited image in a rectangular frame with the gap between album image and frame the distrotion
