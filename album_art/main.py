76e204835960c9d3b520423941bdb861411b45d4
Mouzone <sunnyliu010@gmail.com>
2024-03-15 19:44:30 -0400

    load and print images from album art folder

--- album_art/main.py
+++ album_art/main.py
@@ -1,3 +1,14 @@
 # make it able to take a file name input or some kind of drag and drop website to proccess
+import os
+from PIL import Image
 
+def proccess_image(image_path):
+    img = Image.open(image_path)
+    img.show()
+
+folder_path = "original albums"
+files = os.listdir(folder_path)
+for file in files:
+    image_path = os.path.join(folder_path, file)
+    proccess_image(image_path)
 # output is the edited image in a rectangular frame with the gap between album image and frame the distrotion
